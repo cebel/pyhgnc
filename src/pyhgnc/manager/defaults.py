e3d0cf8d045a57eeb27eb38ce646232e5c939c9e
Andrej Konotopez <andrej.konotopez@scai.fraunhofer.de>
2017-09-06 14:54:22 +0200

    Corrected path in defaults.py

--- src/pyhgnc/manager/defaults.py
+++ src/pyhgnc/manager/defaults.py
@@ -10,7 +10,7 @@ DEFAULT_DATABASE_LOCATION = os.path.join(PYHGNC_DATA_DIR, DEFAULT_SQLITE_DATABAS
 DEFAULT_TEST_DATABASE_LOCATION = os.path.join(PYHGNC_DATA_DIR, DEFAULT_SQLITE_TEST_DATABASE_NAME)
 
 sqlalchemy_connection_string_default = 'sqlite:///' + DEFAULT_DATABASE_LOCATION
-sqlalchemy_connection_string_4_tests = 'sqlite:///' + DEFAULT_SQLITE_TEST_DATABASE_NAME
+sqlalchemy_connection_string_4_tests = 'sqlite:///' + DEFAULT_TEST_DATABASE_LOCATION
 
 sqlalchemy_connection_string_4_mysql = 'mysql+pymysql://pyhgnc:pyhgnc@localhost/pyhgnc?charset=utf8'
 sqlalchemy_connection_string_4_mysql_tests = 'mysql+pymysql://pyhgnc:pyhgnc@localhost/pyhgnc_test?charset=utf8'
