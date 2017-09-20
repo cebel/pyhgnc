fee23370aec85ee3d7b3beaea72f0c576d78170f
Andrej Konotopez <andrej.konotopez@scai.fraunhofer.de>
2017-09-20 17:45:50 +0200

    removed collation for now and automated deploing

--- src/pyhgnc/manager/models.py
+++ src/pyhgnc/manager/models.py
@@ -158,7 +158,7 @@ class HGNC(Base, MasterModel):
     :cvar list enas: relationship to ENA
     """
     name = Column(String(255), nullable=True)
-    symbol = Column(Unicode(255, collation='utf8_bin'), index=True)
+    symbol = Column(Unicode(255), index=True)
     identifier = Column(Integer, unique=True)
     status = Column(String(255))
     uuid = Column(String(255))
@@ -263,7 +263,7 @@ class AliasSymbol(Base, MasterModel):
     :cvar hgnc: back populates to :class:`.HGNC`
     """
 
-    alias_symbol = Column(Unicode(255, collation='utf8_bin'))
+    alias_symbol = Column(Unicode(255))
     is_previous_symbol = Column(Boolean, default=False)
 
     hgnc_id = foreign_key_to('hgnc')
@@ -572,14 +572,14 @@ class OrthologyPrediction(Base, MasterModel):
     human_entrez_gene = Column(Integer)
     human_ensembl_gene = Column(String(255))
     human_name = Column(String(255))
-    human_symbol = Column(Unicode(255, collation='utf8_bin'))
+    human_symbol = Column(Unicode(255))
     human_chr = Column(String(255))
     human_assert_ids = Column(String(255))
     ortholog_species_entrez_gene = Column(Integer)
     ortholog_species_ensembl_gene = Column(String(255))
     ortholog_species_db_id = Column(String(255))
     ortholog_species_name = Column(Text)
-    ortholog_species_symbol = Column(Unicode(255, collation='utf8_bin'), index=True)
+    ortholog_species_symbol = Column(Unicode(255), index=True)
     ortholog_species_chr = Column(String(255))
     ortholog_species_assert_ids = Column(String(255))
     support = Column(String(255))
