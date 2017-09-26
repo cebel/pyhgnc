13a2da39e02e9b1ee8fda8ddb0c77786f6af0671
Andrej Konotopez <andrej.konotopez@scai.fraunhofer.de>
2017-09-26 12:00:25 +0200

    Bump version: 0.1.1 → 0.1.2-dev

--- src/pyhgnc/__init__.py
+++ src/pyhgnc/__init__.py
@@ -21,7 +21,7 @@ query = QueryManager
 
 __all__ = ['update', 'query']
 
-__version__ = '0.1.1'
+__version__ = '0.1.2-dev'
 
 __title__ = 'PyHGNC'
 __description__ = 'Importing and querying HGNC data'
